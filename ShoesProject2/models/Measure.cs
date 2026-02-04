0afa00b6cfb4044455c1a62151d5248d887276a9
User <User@WIN-1STBJGFL3G0>
2026-02-04 13:12:54 +0500

    Установка пакетов, подключение к базе данных.

---
+++ ShoesProject2/models/Measure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoesProject2.models;
+
+public partial class Measure
+{
+    public int Id { get; set; }
+
+    public string MeasureName { get; set; } = null!;
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+}
